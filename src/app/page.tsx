35c351a21e5ec198ed248922fa81755c94bd76b2
Danimoz <azubuinedaniel05@gmail.com>
2023-11-12 13:28:28 +0100

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Vote from '@/components/vote';
 import { getCandidates } from '@/lib/actions'
-import Image from 'next/image'
 
 export default async function Home() {
   const candidates = await getCandidates();
@@ -12,7 +11,6 @@ export default async function Home() {
       <div className='grid grid-cols-1 md:grid-cols-3 mt-6'>
         {!('error' in candidates) && candidates?.map((candidate) => (
           <div key={candidate.id} className='shadow-2xl'>
-            <Image src={candidate.image} alt={candidate.name} width={500} height={500} />
             <div className='p-6'>
               <h2 className='text-xl font-bold'>{candidate.name}</h2>
               <p>{candidate.description}</p>
